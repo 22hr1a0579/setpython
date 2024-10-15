e420d321f1ae967351205b0f0dd8708b92759a55
P Nagamani <154883406+22hr1a0579@users.noreply.github.com>
2024-10-15 10:30:48 +0530

    Create diffval.py

---
+++ diffval.py
@@ -0,0 +1,14 @@
+''' Write a program to print only the different values between two given sets.
+Sample Input:
+1 2 3 4
+2 4 6 8
+Sample Output:
+1 3'''
+
+set1_input = input().strip().split()
+set2_input = input().strip().split()
+set1 = set(map(int, set1_input))
+set2 = set(map(int, set2_input))
+difference = set1.difference(set2)
+sorted_difference = sorted(difference)
+print(" ".join(map(str, sorted_difference)))
